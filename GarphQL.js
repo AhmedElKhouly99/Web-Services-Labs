e13005634dff446550f0490f655a4fb6da7e55f8
AhmedElKhouly99 <96910732+AhmedElKhouly99@users.noreply.github.com>
2022-06-03 23:49:55 +0200

    Add files via upload

--- GarphQL.js
+++ GarphQL.js
@@ -2,32 +2,47 @@ const { ApolloServer, gql } = require('apollo-server');
 
 
 let users = [
-    { id: 1, firstName: 'ahmed',lastName: 'ahmed',
-     email:'email',isSuspended:true,dob:'11-2-2010' },
-     { id: 2, firstName: 'Adel',lastName: 'ahmed',
-     email:'email',isSuspended:true,dob:'11-2-2010' },
-     { id: 3, firstName: 'Mina',lastName: 'ahmed',
-     email:'email',isSuspended:true,dob:'11-2-2010' }
-  
+    {
+        id: 1, firstName: 'ahmed', lastName: 'ahmed',
+        email: 'email', isSuspended: true, dob: '11-2-2010'
+    },
+    {
+        id: 2, firstName: 'Adel', lastName: 'ahmed',
+        email: 'email', isSuspended: true, dob: '11-2-2010'
+    },
+    {
+        id: 3, firstName: 'Mina', lastName: 'ahmed',
+        email: 'email', isSuspended: true, dob: '11-2-2010'
+    }
+
 ];
 let comments = [
-    {  id: 1, content: "Comment 1",
-        user:users[1],
-        date: '12-2-2010' },
-   
-  
+    {
+        id: 1,
+        content: "Comment 1",
+        user: users[1],
+        date: '12-2-2010'
+    },
+
+
 ];
 let articles = [
-    { id: 1, title: 'article1',
-    body:'sadasd fdsf edad',date:'11-2-2010',
-    author:users[0],comments:[comments[0]] },
-    { id: 2, title: 'article2',
-    body:'sadasd fdsf edad',date:'11-2-2010',
-    author:users[0],comments:[comments[0]] },
-    { id: 3, title: 'article3',
-    body:'sadasd fdsf edad',date:'11-2-2010',
-    author:users[0],comments:[comments[0]] }
-   
+    {
+        id: 1, title: 'article1',
+        body: 'cfegshty', date: '11-2-2010',
+        author: users[0], comments: [comments[0]]
+    },
+    {
+        id: 2, title: 'article2',
+        body: 'byrenbdhry', date: '11-2-2010',
+        author: users[0], comments: [comments[0]]
+    },
+    {
+        id: 3, title: 'article3',
+        body: 'ryhdtgs', date: '11-2-2010',
+        author: users[0], comments: [comments[0]]
+    }
+
 ];
 
 
@@ -76,22 +91,23 @@ const typeDefs = gql(Schema);
 const resolvers = {
     Query: {
         allUsers: () => users,
-        allArticles: ()=> articles,
-        allComments: ()=> comments
+        allArticles: () => articles,
+        allComments: () => comments
     },
     Mutation: {
         deleteArticle: (_, { id }) => {
             articles = articles.filter((article) => article.id !== id);
             return articles;
         },
-        createArticle: (_, { id,  title,
+        createArticle: (_, { id, title,
             body,
             author }) => {
-            articles.push({id,
+            articles.push({
+                id,
                 title,
                 body,
                 author: users.filter((user) => user.id === author)[0],
-                });
+            });
             return articles;
         }
     }
